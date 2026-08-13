358a5ab4491f8f0b3c91dbe6d0c924fc4c9fcf36
admin <admin@CE-106.siesgst.local>
2026-08-13 14:48:39 +0530

    bug fixing

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,11 +1,14 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    service = Service(r"C:\WebDriver\chromedriver.exe")
+
+    driver = webdriver.Chrome(service=service)
     driver.maximize_window()
 
     yield driver
@@ -18,6 +21,7 @@ def test_valid_login(driver):
 
     driver.find_element(By.ID, "user-name").send_keys("standard_user")
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
+
     driver.find_element(By.ID, "login-button").click()
 
     assert "inventory" in driver.current_url
@@ -28,8 +32,12 @@ def test_invalid_login(driver):
 
     driver.find_element(By.ID, "user-name").send_keys("invalid_user")
     driver.find_element(By.ID, "password").send_keys("invalid_password")
+
     driver.find_element(By.ID, "login-button").click()
 
-    error = driver.find_element(By.CSS_SELECTOR, "[data-test='error']")
+    error = driver.find_element(
+        By.CSS_SELECTOR,
+        "[data-test='error']"
+    )
 
     assert error.is_displayed()
